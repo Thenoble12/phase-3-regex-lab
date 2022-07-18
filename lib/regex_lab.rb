d4d520db1dc6698fdeb83480d9bd6782ed2abaa9
Elijah Noble <elijahnobledev@gmail.com>
2022-07-17 19:05:36 -0700

    Assiginment incomplete

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+    puts word
+    puts word.match(/\A[aeiou]/)
+    word.downcase.match(/\A[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    puts(text)
+    puts text.match(/\A(un)|(ing)\z/)
+    text.downcase.grep(/\A(un)|(ing)\z/) ? true : false
 end
 
 def words_five_letters_long(text)
