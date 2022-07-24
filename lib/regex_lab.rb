4172f8171f363394acb0239542b276c575366d36
Elijah Noble <elijahnobledev@gmail.com>
2022-07-23 17:15:21 -0700

    Assiginment Advanced

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,20 @@
 def starts_with_a_vowel?(word)
-    puts word
-    puts word.match(/\A[aeiou]/)
     word.downcase.match(/\A[aeiou]/) ? true : false
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-    puts(text)
-    puts text.match(/\A(un)|(ing)\z/)
-    text.downcase.grep(/\A(un)|(ing)\z/) ? true : false
+def words_starting_with_un_and_ending_with_ing(text)    
+    text.scan(/un\w*ing\b/)    
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(text)    
+    text.scan(/\b[a-zA-Z]{5}\b/)  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/^[A-Z].*\W$/) ? true : false
 end
 
-def valid_phone_number?(phone)
 
+def valid_phone_number?(phone)
+    phone.scan(/((\+*)((0[ -]*)*|((91 )*))((\d{12})+|(\d{10})+))|\d{5}([- ]*)\d{6}/) ? true : false
 end
